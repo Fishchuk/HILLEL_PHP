802abe0993ae1fdf4eb62d74605bd995a1d7846f
Tatyana Fishchuk <fishchuk.tatyana@gmail.com>
2020-06-04 08:16:38 +0300

    comment out api.php channels.php console.php

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -18,31 +18,33 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::middleware('admin')->prefix('admin')->name('admin.')->group(function (){
+Route::middleware('auth','admin')->prefix('admin')->name('admin.')->group(function (){
     Route::get('/','Admin@index')->name('dashboard');
     Route::prefix('products')->name('products.')->group(function (){
-        Route::put('create/{id}','ProductController@create')->name('create')->where(['id'=>'[0-9]+']);
+        Route::put('/{id}','ProductController@create')->name('create');
         Route::get('/','ProductController@index')->name('index');
-        Route::put('update/{id}','ProductController@update')->name('update')->where(['id'=>'[0-9]+']);
-        Route::delete('delete/{id}','ProductController@delete')->name('delete')->where(['id'=>'[0-9]+']);
+        Route::post('/','ProductController@store')->name('store');
+        Route::put('/{id}','ProductController@update')->name('update');
+        Route::delete('/{id}','ProductController@destroy')->name('destroy');
     });
     Route::prefix('orders')->name('orders.')->group(function (){
-        Route::put('create/{id}','OrdersController@create')->name('create')->where(['id'=>'[0-9]+']);
+        Route::put('/{id}','OrdersController@create')->name('create');
         Route::get('/','OrdersController@index')->name('index');
-        Route::put('update/{id}','OrdersController@update')->name('update')->where(['id'=>'[0-9]+']);
-        Route::delete('delete/{id}','OrdersController@delete')->name('delete')->where(['id'=>'[0-9]+']);
+        Route::post('/','OrdersController@store')->name('store');
+        Route::put('/{id}','OrdersController@update')->name('update');
+        Route::delete('/{id}','OrdersController@destroy')->name('destroy');
     });
     Route::prefix('categories')->name('categories.')->group(function (){
-        Route::put('create/{id}','CategoriesController@create')->name('create')->where(['id'=>'[0-9]+']);
+        Route::put('/{id}','CategoriesController@create')->name('create');
         Route::get('/','CategoriesController@index')->name('index');
-        Route::get('{id}/categories', 'CategoriesController@show')->name('show');
-        Route::get('categories/{id}/edit', 'CategoriesController@edit')->name('edit')->where(['id'=>'[0-9]+']);
-        Route::put('update/{id}','CategoriesController@update')->name('update')->where(['id'=>'[0-9]+']);
-        Route::delete('delete/{id}','CategoriesController@delete')->name('delete')->where(['id'=>'[0-9]+']);
+        Route::post('/', 'CategoriesController@show')->name('store');
+        Route::get('/{id}/edit', 'CategoriesController@edit')->name('edit');
+        Route::put('/{id}','CategoriesController@update')->name('update');
+        Route::delete('/{id}','CategoriesController@destroy')->name('destroy');
     });
     Route::prefix('users')->name('users.')->group(function (){
         Route::get('/','UsersController@index')->name('index');
-        Route::get('{id}/users', 'UsersController@show')->name('show');
-        Route::delete('delete/{id}','UsersController@delete')->name('delete')->where(['id'=>'[0-9]+']);
+        Route::get('{id}', 'UsersController@show')->name('show');
+        Route::delete('/{id}','UsersController@destroy')->name('destroy');
     });
 });
